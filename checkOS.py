eecb4aa931f57829890319ad7a1a923e1bda10b7
Prince Thapa <princethapa11@protonmail.com>
2022-02-10 14:48:50 +0530

    Add files via upload

---
+++ checkOS.py
@@ -0,0 +1,10 @@
+# 7 Aug 2020
+# To check wich OS it is
+
+import os
+import platform
+
+if platform.system() == 'Linux':
+   print("platform is " + platform.system())
+elif platform.system() == 'Windows':
+   print("platform is " + platform.system())
