f15c011e87a083aa6af56cea02d70c3d08ccc08e
Prince Thapa <princethapa11@protonmail.com>
2022-02-10 14:49:49 +0530

    Add files via upload

---
+++ taking_input.py
@@ -0,0 +1,14 @@
+# 4 april 2021 Week 14 Sunday
+# taking input from user
+
+name = input('What is your name?\n')
+print('Hi ' + name) 
+
+fcolor = input("What is your favourite color?\n")
+
+if fcolor == "green":
+    print("Who the fuck likes green color")
+elif fcolor == "pink":
+    print("Cute color")
+else:
+    print("Favourite color of " + name + " is : " + fcolor) 
